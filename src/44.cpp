23d0e6effcb2f98b06f21b8b643beb79d409e1f1
platzdoudouche <platzdoudouche@gmail.com>
2025-05-26 22:09:57 +0000

    Add 44.cpp

---
+++ src/44.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 5;
+    if (num % 2 == 0) {
+        cout << "The number is even." << endl;
+    } else {
+        cout << "The number is odd." << endl;
+    }
+    return 0;
+}
